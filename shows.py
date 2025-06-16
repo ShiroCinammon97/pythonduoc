66177ddc848b4f48b8b9d7ec66f8dc97b3de7395
ShiroCinammon97 <capnpncake@gmail.com>
2025-06-16 19:22:26 -0400

    avance shows.py

--- shows.py
+++ shows.py
@@ -1,3 +1,24 @@
+'''
+Funcionalidades
+1. Artistas por país
+- Función: artistas_por_pais(pais)
+- Muestra una lista ordenada de artistas que se presentan en el país ingresado.
+- Si no hay artistas del país: "No hay artistas de ese país."
+2. Porcentaje de shows en un mes
+- Función: shows_por_mes(mes)
+- Valida que el mes esté entre 1 y 12.
+- Devuelve el porcentaje de shows en ese mes (redondeado a un decimal).
+3. Eliminar artista por nombre
+- Función: eliminar_artista()
+- Solicita nombre del artista y lo elimina si existe (sin importar mayúsculas/minúsculas).
+
+ Mensajes posibles:
+- "Artista eliminado con éxito."
+- "Artista no encontrado. No se pudo eliminar."
+
+'''
+
+
 usuarios = {"admin": "Admin*2025"}
 
 shows = {
@@ -11,3 +32,18 @@ shows = {
     "A008": ["Karol G", "Colombia", "02-12-2025"],
     "A009": ["The Weeknd", "Canadá", "30-06-2025"]
 }
+
+def inicio_sesion():
+    pass
+
+def artistas_por_pais():
+    pass
+
+def shows_por_mes():
+    pass
+
+def eliminar_artista():
+    pass
+
+for id in shows:
+    print(id)
