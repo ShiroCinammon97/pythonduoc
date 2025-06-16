ff78bea65edff7ee570185d668211c2569c7802d
ShiroCinammon97 <capnpncake@gmail.com>
2025-06-16 19:49:06 -0400

    Avance

--- shows.py
+++ shows.py
@@ -1,4 +1,16 @@
 '''
+inicio sesion
+ -Se solicita nombre de usuario y contraseña.
+- El usuario debe haber sido registrado previamente.
+- Si las credenciales son incorrectas, no se podrá ingresar.
+
+Menú del sistema
+*** SISTEMA DE SHOWS EN VIVO ***
+1. Mostrar artistas por país
+2. Porcentaje de shows en un mes
+3. Eliminar artista por nombre
+4. Salir
+
 Funcionalidades
 1. Artistas por país
 - Función: artistas_por_pais(pais)
@@ -17,7 +29,8 @@ Funcionalidades
 - "Artista no encontrado. No se pudo eliminar."
 
 '''
-
+login = False
+contador_login = 0
 
 usuarios = {"admin": "Admin*2025"}
 
@@ -33,11 +46,23 @@ shows = {
     "A009": ["The Weeknd", "Canadá", "30-06-2025"]
 }
 
-def inicio_sesion():
-    pass
+def verificar_passwd (texto):
+    upper = False
+    if len(texto) < 9:
+        print("Contraseña debe contener 9 o más caracteres")
+        return False
+    for letra in texto:
+        if letra.isupper():
+            upper = True
+    if not upper:
+        print("La contraseña debe contener almenos una mayúscula")
+        return False
+    
 
-def artistas_por_pais():
+def artistas_por_pais(pais):
+#    for evento in shows:
     pass
+        
 
 def shows_por_mes():
     pass
@@ -45,5 +70,43 @@ def shows_por_mes():
 def eliminar_artista():
     pass
 
-for id in shows:
-    print(shows[id][0])
+'''for id in shows:
+    print(shows[id][0])'''
+
+
+print("Bienvenido al sistema conciertos")
+
+while True:
+    opcion_login = int(input("Inserte opción:1.Login2.Registro"))
+    usuario = input("Inserte nombre de usuario")
+    contrasena = input("Inserte contraseña de usuario")
+    for usuario in usuarios:
+        if usuarios[usuario] == contrasena:
+            login = True
+        else:
+            print("Credenciales incorrectas")
+    contador_login += 1
+    if login == True:
+        print("Bienvenido")
+        break
+    if contador_login == 3:
+        break
+    
+
+
+if login == False:
+    print("Cuenta bloqueada")
+else:
+    while True:
+        opcion = int(input("Ingrese opción\n1. Mostrar artistas por país\n2. Porcentaje de shows en un mes\n3. Eliminar artista por nombre\n4. Salir\n"))
+        if opcion == 1:
+            artistas_por_pais()
+        elif opcion == 2:
+            shows_por_mes()
+        elif opcion == 3:
+            eliminar_artista()
+        elif opcion == 4:
+            print("Adios")
+            break
+        else:
+            print("inserte opción váldia")
