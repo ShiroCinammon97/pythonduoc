1611a8074a014e3777c69a9dbf3d224797fd188a
ShiroCinammon97 <capnpncake@gmail.com>
2025-06-17 09:09:52 -0400

    Agregada version autonoma

---
+++ showsV2.py
@@ -0,0 +1,13 @@
+usuarios = {"admin": "Admin*2025"}
+
+shows = {
+    "A001": ["Taylor Swift", "Estados Unidos", "15-09-2025"],
+    "A002": ["Bad Bunny", "Puerto Rico", "03-08-2025"],
+    "A003": ["Rosalía", "España", "21-10-2025"],
+    "A004": ["BLACKPINK", "Corea del Sur", "05-07-2025"],
+    "A005": ["Dua Lipa", "Reino Unido", "18-08-2025"],
+    "A006": ["BTS", "Corea del Sur", "12-09-2025"],
+    "A007": ["Shakira", "Colombia", "25-11-2025"],
+    "A008": ["Karol G", "Colombia", "02-12-2025"],
+    "A009": ["The Weeknd", "Canadá", "30-06-2025"]
+} 
