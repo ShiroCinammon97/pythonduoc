385474c6026204aeda6669af8fd2f810396c22f5
ShiroCinammon97 <145036409+ShiroCinammon97@users.noreply.github.com>
2025-06-20 19:15:28 -0400

    Nuevo Ejercicio

---
+++ registro_contactos.py
@@ -0,0 +1,15 @@
+'''
+🧩 Ejercicio: Sistema de registro interactivo de contactos
+Crea un programa que simule un sistema de gestión de contactos. El usuario puede:
+
+Agregar un nuevo contacto
+
+Ver todos los contactos
+
+Buscar por nombre
+
+Salir del programa
+
+Cada contacto se guarda como un diccionario con las claves: "nombre", "teléfono" y "email". Todos los contactos se almacenan en una lista.
+'''
+
