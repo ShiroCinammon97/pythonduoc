eadd607c2566817ad887950f522d0a72415f81cb
ShiroCinammon97 <145036409+ShiroCinammon97@users.noreply.github.com>
2025-06-22 12:43:40 -0400

    Nuevo ejercicio para practicar listas y diccionarios

---
+++ registro_repetido.py
@@ -0,0 +1,18 @@
+'''
+Ejercicio: Detectar registros duplicados según múltiples campos
+Crea un programa que permita ingresar múltiples personas (una por línea), con su nombre y correo electrónico. Cada entrada debe almacenarse como un diccionario en una lista.
+
+El programa debe detectar si ya existe una persona con el mismo nombre o con el mismo correo, e impedir su registro duplicado.
+
+Requisitos:
+Usar solo listas y diccionarios.
+
+Detectar duplicados por nombre o por correo.
+
+Mostrar un mensaje si se detecta duplicado.
+
+El programa termina cuando se ingresa "FIN" como nombre.
+
+
+'''
+
