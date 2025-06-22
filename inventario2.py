b3fae0b7daa5a4d3ed653fa13d57a203b007f3db
ShiroCinammon97 <145036409+ShiroCinammon97@users.noreply.github.com>
2025-06-22 13:52:55 -0400

    Falta agregar mostrar productos por categoría

--- inventario2.py
+++ inventario2.py
@@ -12,3 +12,59 @@ El programa debe permitir al usuario consultar cuántos productos hay por catego
 🔁 El programa termina cuando el usuario escribe "FIN" como nombre de producto.
 '''
 
+productos = []
+
+def verificar_codigo(codigo):
+    num = False
+    letra = False
+    if len(productos) == 0:
+        pass
+    else:
+        for producto in productos:
+            if producto["Código"] == codigo:
+                print("Código ya existe")
+                return False
+    
+    if len(codigo) < 4:
+        print("El código debe ser de almenos 4 caracteres")
+        return False
+    
+    for caracter in codigo:
+        if caracter.isnumeric():
+            num = True
+        if caracter.isalpha():
+            letra = True
+    
+    if num == True and letra == True:
+        return True
+    else:
+        print("El código debe tener almenos una letra y un número")
+        return False
+
+def agregar_producto(nombre,codigo,categoria):
+    productos.append({"Nombre":nombre,"Código":codigo,"Categoría":categoria})
+
+while True:
+    try:
+        opcion = int(input("Inserte una opcion:\n1. Agregar Producto\n2. Ver Productos por categoría\n3. Salir\n"))
+    except ValueError as error:
+        print("Error: ",error)
+        print("Se esperaba un número entero")
+        opcion = 0
+    if opcion == 1:
+        nombre = input("Inserte el nombre del producto\n")
+        while True:
+            codigo = input("Inserte el código del producto\n")
+            if verificar_codigo(codigo) == True:
+                break
+        categoria = input("Inserte el categoría del producto\n")
+        agregar_producto(nombre,codigo,categoria)
+    elif opcion == 2:
+        pass
+    elif opcion == 3:
+        print("Adios")
+        break
+    elif opcion == 0:
+        print("Ingrese un número entero")
+    else:
+        print("Ingrese una opción válida")
