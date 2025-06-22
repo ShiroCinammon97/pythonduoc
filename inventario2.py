661d0e0f013c676116eef93b4123af16ed729385
ShiroCinammon97 <145036409+ShiroCinammon97@users.noreply.github.com>
2025-06-22 13:27:12 -0400

    Nuevo ejercicio

---
+++ inventario2.py
@@ -0,0 +1,14 @@
+'''
+Crea un programa que administre un inventario de productos, donde cada producto tenga:
+un nombre
+un código (alfanumérico, obligatorio y único)
+una categoría
+Los datos se deben ingresar de uno en uno, y almacenarse en una lista de diccionarios.
+
+📌 Reglas:
+No se permite registrar dos productos con el mismo código.
+El programa debe validar que el código tenga al menos 4 caracteres y al menos una letra y un número.
+El programa debe permitir al usuario consultar cuántos productos hay por categoría al final.
+🔁 El programa termina cuando el usuario escribe "FIN" como nombre de producto.
+'''
+
